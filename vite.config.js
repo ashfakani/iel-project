72bace00e6475027fb6067de5d00b4c10ae92c2d
ashfakani <ashfakani75@gmail.com>
2025-08-26 18:08:37 +0600

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/iel-project/', // 👈 Required for GitHub Pages
   plugins: [react(), tailwindcss()],
 })
